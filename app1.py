1aa16ef69d2b2cf88c058c927ac25782a2f46f55
vishwamm <128782030+vishwamm@users.noreply.github.com>
2023-08-26 08:55:57 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+import os
+
 
 app1 = Flask(__name__)
 
